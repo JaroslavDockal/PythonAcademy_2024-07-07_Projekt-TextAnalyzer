1126d810b1367c13a0f4d1079ea53c8a0857f811
JaroslavDockal <157100203+JaroslavDockal@users.noreply.github.com>
2024-09-15 14:13:10 +0200

    Debug disabled

--- TextAnalyzer.py
+++ TextAnalyzer.py
@@ -11,23 +11,27 @@ discord: jaroslav.dockal
 import hashlib
 import re
 
-DEBUG = True
+DEBUG = False
+
 
 def debug_print(message):
     if DEBUG:
         print(f"Debug: {message}")
 
+
 users = {
     'bob': '202cb962ac59075b964b07152d234b70',
-    'ann': '482c811da5d5b4bc6d497ffa98491e38',
+    'ann': '32250170a0dca92d53ec9624f336ca24',
     'mike': '482c811da5d5b4bc6d497ffa98491e38',
-    'liz': '482c811da5d5b4bc6d497ffa98491e38'
+    'liz': '32250170a0dca92d53ec9624f336ca24'
 }
+
 
 def hash_password(password):
     hashed = hashlib.md5(password.encode()).hexdigest()
     debug_print(f"hashed password is {hashed}")
     return hashed
+
 
 def authenticate(username, password):
     if username in users:
@@ -40,6 +44,7 @@ def authenticate(username, password):
     else:
         debug_print("user not found")
     return False
+
 
 def select_text():
     print("We have 3 texts to be analyzed.")
@@ -55,28 +60,35 @@ def select_text():
         print("Invalid input. Terminating the program...")
         exit()
 
-TEXTS = [
-    """Situated about 10 miles west of Kemmerer, 
-    Fossil Butte is a ruggedly impressive topographic feature that rises 
-    sharply some 1000 feet above Twin Creek Valley to an elevation of more 
-    than 7500 feet above sea level. The butte is located just north of US 
-    30N and the Union Pacific Railroad, which traverse the valley.""",
 
-    """At the base of Fossil Butte are the bright red, purple, yellow and 
-    gray beds of the Wasatch Formation. Eroded portions of these horizontal 
-    beds slope gradually upward from the valley floor and steepen abruptly. 
-    Overlying them and extending to the top of the butte are the much steeper 
-    buff-to-white beds of the Green River Formation, which are about 300 feet 
-    thick.""",
-
-    """The monument contains 8198 acres and protects a portion of the largest 
-    deposit of freshwater fish fossils in the world. The richest fossil 
-    fish deposits are found in multiple limestone layers, which lie some 
-    100 feet below the top of the butte. The fossils represent several 
-    varieties of perch, as well as other freshwater genera and herring 
-    similar to those in modern oceans. Other fish such as paddlefish, 
-    garpike and stingray are also present."""
+TEXTS = ['''
+Situated about 10 miles west of Kemmerer,
+Fossil Butte is a ruggedly impressive
+topographic feature that rises sharply
+some 1000 feet above Twin Creek Valley
+to an elevation of more than 7500 feet
+above sea level. The butte is located just
+north of US 30N and the Union Pacific Railroad,
+which traverse the valley. ''',
+'''At the base of Fossil Butte are the bright
+red, purple, yellow and gray beds of the Wasatch
+Formation. Eroded portions of these horizontal
+beds slope gradually upward from the valley floor
+and steepen abruptly. Overlying them and extending
+to the top of the butte are the much steeper
+buff-to-white beds of the Green River Formation,
+which are about 300 feet thick.''',
+'''The monument contains 8198 acres and protects
+a portion of the largest deposit of freshwater fish
+fossils in the world. The richest fossil fish deposits
+are found in multiple limestone layers, which lie some
+100 feet below the top of the butte. The fossils
+represent several varieties of perch, as well as
+other freshwater genera and herring similar to those
+in modern oceans. Other fish such as paddlefish,
+garpike and stingray are also present.'''
 ]
+
 
 def analyze_text(text):
     words = re.findall(r'\b\w+\b', text)
@@ -109,9 +121,9 @@ def analyze_text(text):
     for length in sorted(length_freq):
         print(f"{length:>3}| {'*' * length_freq[length]:<12} |{length_freq[length]}")
 
+
 def main():
     username = input("username: ")
-    #TODO Jak skrýt heslo na vstupu?
     password = input("password: ")
 
     if authenticate(username, password):
@@ -121,6 +133,7 @@ def main():
         analyze_text(text)
     else:
         print("Unregistered user, terminating the program...")
+
 
 if __name__ == "__main__":
     main()
